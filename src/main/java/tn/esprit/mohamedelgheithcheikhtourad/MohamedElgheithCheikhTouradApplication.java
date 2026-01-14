0228a4b43d7373415d3b40ca3df5da3df3ee0944
Cheikhtourad19 <elghothvadel@gmail.com>
2026-01-14 11:32:41 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/mohamedelgheithcheikhtourad/MohamedElgheithCheikhTouradApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.mohamedelgheithcheikhtourad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohamedElgheithCheikhTouradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohamedElgheithCheikhTouradApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/mohamedelgheithcheikhtourad/MohamedElgheithCheikhTouradApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.mohamedelgheithcheikhtourad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohamedElgheithCheikhTouradApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
